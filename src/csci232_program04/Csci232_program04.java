1fab3f5b7bb7edbc95dae16c0c5f37023129ed22
Selene Smith <seleneasmith@gmail.com>
2018-04-11 10:24:17 -0600

    cause i followed the tutorial for lab 5 again....

---
+++ src/csci232_program04/Csci232_program04.java
@@ -0,0 +1,15 @@
+package csci232_program04;
+
+/**
+ *
+ * @author Sell
+ */
+
+public class Csci232_program04 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
